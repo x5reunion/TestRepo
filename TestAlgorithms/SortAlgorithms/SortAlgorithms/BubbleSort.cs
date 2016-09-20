3fb715909425fdcad99b94e9d6999a53e9f38856
x5reunion <x5reunion@yandex.ru>
2016-09-20 22:55:48 +0300

    TestAlgorithms
    Sort, Palindrom and other

---
+++ TestAlgorithms/SortAlgorithms/SortAlgorithms/BubbleSort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortAlgorithms
+{
+    public class BubbleSort
+    {
+        // https://ru.wikipedia.org/wiki/Сортировка_пузырьком
+
+        public static void Sort(int[] data)
+        {
+            bool next;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                next = false;
+
+                // Так как после каждого прохода наибольшее число всегда будет в конце,
+                // то с каждым разом снижаем количество ходов на i.
+
+                for (int j = 0; j < data.Length - i - 1; j++)
+                {
+                    if (data[j] > data[j + 1])
+                    {
+                        Swap(ref data[j], ref data[j + 1]);
+                        next = true;
+                    }
+                }
+
+                //foreach (var n in data)
+                //{
+                //    Console.Write("{0} ", n);
+                //}
+
+                //Console.WriteLine();
+
+                if (!next)
+                    break;
+            }
+        }
+
+        private static void Swap<T>(ref T one, ref T two)
+        {
+            T temp = one;
+            one = two;
+            two = temp;
+        }
+    }
+}
---
+++ TestAlgorithms/SortAlgorithms/SortAlgorithms/HeapSort.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortAlgorithms
+{
+    public class HeapSort
+    {
+        // Не реализован. Принцип работы неясен.
+        // https://ru.wikipedia.org/wiki/Пирамидальная_сортировка
+
+        public static void Sort(int[] data)
+        {
+            //for (int i = data.Length / 2 - 1; i >= 0; i--)
+            //{
+            //    ShiftDown(data, i, data.Length);
+            //}
+
+            //for (int i = data.Length - 1; i >= 1; i--)
+            //{
+            //    Swap(ref data[0], ref data[i]);
+            //    ShiftDown(data, 0, i);
+
+            //    foreach (var n in data)
+            //    {
+            //        Console.Write("{0} ", n);
+            //    }
+
+            //    Console.WriteLine();
+            //}
+        }
+
+        private static void ShiftDown(int[] data, int i, int j)
+        {
+            bool done = false;
+            int maxChild = 0;
+
+            while ((i * 2 + 1 < j) && (!done))
+            {
+                if (i * 2 + 1 == j - 1)
+                    maxChild = i * 2 + 1;
+                else if (data[i * 2 + 1] > data[i * 2 + 2])
+                    maxChild = i * 2 + 1;
+                else
+                    maxChild = i * 2 + 2;
+
+                if (data[i] < data[maxChild])
+                {
+                    Swap(ref data[i], ref data[maxChild]);
+
+                    i = maxChild;
+                }
+                else
+                {
+                    done = true;
+                }
+            }
+        }
+
+        private static void Swap<T>(ref T one, ref T two)
+        {
+            T temp = one;
+            one = two;
+            two = temp;
+        }
+    }
+}
