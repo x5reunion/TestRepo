cd2f59d52ee504f6f714b51d90fa45352e1a669a
x5reunion <x5reunion@yandex.ru>
2016-09-20 23:00:53 +0300

    Revert "Revert "TestAlgorithms""
    This reverts commit 86b4dd997977e917d57b510a4a39bd9fb79ba717.

---
+++ TestAlgorithms/SortAlgorithms/SortAlgorithms/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Numerics;
+
+namespace SortAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //BigInteger myValue = 23;// new BigInteger();
+
+            //Console.WriteLine(myValue);
+
+            //BigInteger.Parse
+
+            //int[] data = new int[] { 44, 55, 12, 42, 94, 18, 06, 67 };//{ 16, 11, 9, 10, 5, 6, 8, 1, 2, 4 };
+
+            //foreach (var n in data)
+            //{
+            //    Console.Write("{0} ", n);
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            ////BubbleSort.Sort(data);
+            //HeapSort.Sort(data);
+
+            //Console.WriteLine();
+
+            //foreach (var n in data)
+            //{
+            //    Console.WriteLine(n);
+            //}
+
+            //Console.WriteLine(int.Parse(new string(24.ToString().Reverse().ToArray())));
+
+            //PalindromA.ShowPalindromsCount(12148);
+
+            //List<ulong> mass = new List<ulong>()
+            //{
+            //    196, 295, 394, 493,
+            //    592, 689, 691, 788,
+            //    790, 879, 887, 978,
+            //    986, 1495, 1497,
+            //    1585, 1587, 1675,
+            //    1677, 1765, 1767,
+            //    1855, 1857, 1945,
+            //    1947, 1997, 2494,
+            //    2496, 2584, 2586,
+            //    2674, 2676, 2764,
+            //    2766, 2854, 2856,
+            //    2944, 2946, 2996,
+            //    3493, 3495, 3583,
+            //    3585, 3673, 3675,
+
+            //    //3763, 3765, 3853, 3855
+            //};
+
+            //for (ulong i = 0; i <= 12148; i++)
+            //{
+            //    //if (!mass.Contains(i))
+            //    //{
+            //        Console.WriteLine();
+            //        Console.WriteLine("VALUE = {0}", i);
+            //        Console.WriteLine();
+            //        PalindromA.HavePalindrom(i);
+            //    //}
+
+            //}
+
+            //PalindromA.ShowPalindromsCount(12148);
+
+            //NumberLowerValue.Show(4, 5);
+
+
+            Console.WriteLine(BigInteger.Parse("023"));
+
+        }
+    }
+}
